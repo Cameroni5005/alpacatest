990d5002794354b8fdc3cebf2a5882e4db9d40b3
Cameroni5005 <111313090+Cameroni5005@users.noreply.github.com>
2025-12-17 22:06:02 -0800

    Update test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ def trade_loop():
             if account.status == "ACTIVE" and not account.trading_blocked:
                 qty = 1
                 print(f"would buy {qty} share of AAPL")
-                # api.submit_order(symbol="AAPL", qty=qty, side='buy', type='market', time_in_force='day')
+                api.submit_order(symbol="AAPL", qty=qty, side='buy', type='market', time_in_force='day')
         except Exception as e:
             print("error:", e)
         time.sleep(60)
@@ -32,3 +32,4 @@ if __name__ == "__main__":
     Thread(target=trade_loop, daemon=True).start()  # start the bot in background
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
